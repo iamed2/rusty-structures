7d4cd124326ac881d872868d68a2f0f8d9639b33
Eric Davies <iamed2@gmail.com>
2016-04-17 22:46:30 -0500

    BinaryHeap simple pass-through ops

--- src/binaryheap/mod.rs
+++ src/binaryheap/mod.rs
@@ -1,6 +1,38 @@
+pub struct BinaryHeap {
+    values: Vec<i64>,
+}
+
+impl BinaryHeap {
+    fn new() -> BinaryHeap {
+        BinaryHeap {
+            values: Vec::new()
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.values.is_empty()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn construction() {
+        let x = BinaryHeap::new();
+        assert!(x.is_empty());
+        assert_eq!(x.len(), 0);
+    }
+
+    #[test]
+    fn mut_construction() {
+        let mut x = BinaryHeap::new();
+        assert!(x.is_empty());
+        assert_eq!(x.len(), 0);
     }
 }
